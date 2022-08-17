311822792c61ba45a1127673249474c8a2f15922
junsircoding <junsircoding@icloud.com>
2022-08-17 22:30:58 +0800

    shell 指定月份的首末日

--- 16-Shell/00-时间相关.sh
+++ 16-Shell/00-时间相关.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-
+# set -x
 # 获取当前时间, 并格式化 
 echo `date "+%Y%m%d%H%M"` 
 # 202208111428 
@@ -9,3 +9,25 @@ echo `date --date "+1 day" "+%Y%m%d%H%M"`
 
 # 时间加一分钟 
 echo `date --date "+1 min" "+%Y%m%d%H%M"` 
+
+# 获取指定月份的第一天和最后一天
+# 首个参数不传置为当天日期
+if [ "x$1" == "x" ] ; then
+    #date=$(whichtime3 glb)
+    date_m=$(date +%Y%m)
+else
+    date_m=$1
+fi
+date_m=202206
+date_m_1=`date -d "${date_m:0:6}01 +1 month" +%Y%m%d`
+echo $date_m_1
+# 指定月首日
+first_d=${date_m:0:6}01
+echo $first_d
+first_d_1=${date_m_1:0:6}01
+echo $first_d_1
+# 指定月末日
+last_d=`date -d "${first_d_1:0:6}01 -1 day" +%Y%m%d`
+echo $last_d
+
+echo $date_m"首日:"$first_d", 末日:"$last_d
